073a80bed2567bd72552a50186b1ae5a80758dd8
Abdel Droid <106479409+abdessattar23@users.noreply.github.com>
2023-02-19 22:02:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals(sendToVercelAnalytics);
+ReactDOM.render(<App />, document.getElementById("root"));
