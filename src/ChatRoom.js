0d070721b62db62ecbe25fdaf0878b6eb9ad67ba
Abdel Droid <106479409+abdessattar23@users.noreply.github.com>
2023-02-19 23:04:27 +0100

    Update ChatRoom.js

--- src/ChatRoom.js
+++ src/ChatRoom.js
@@ -1,7 +1,53 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { database } from "./firebase";
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-function ChatRoom() {
-  const { chatRoomId } = useParams();
-  const [messages, set
+function Chatroom({ match }) {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const db = firebase.database().ref(match.params.id);
+
+    db.on('value', (snapshot) => {
+      if (snapshot.exists()) {
+        setMessages(snapshot.val());
+      } else {
+        setMessages([]);
+      }
+    });
+
+    return () => db.off();
+  }, [match.params.id]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const db = firebase.database().ref(match.params.id);
+    db.push({
+      text: message,
+      timestamp: Date.now()
+    });
+    setMessage('');
+  }
+
+  return (
+    <div>
+      <h1>Chatroom {match.params.id}</h1>
+      <ul>
+        {messages.map((msg) => (
+          <li key={msg.timestamp}>{msg.text}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default Chatroom;
